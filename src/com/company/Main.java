8a8596c55eb87ff64bf13c6f6e992621d46f1eb3
BT_1N3_04@bktech.local <BT_1N3_04@bktech.local>
2018-10-10 08:44:03 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner num = new Scanner (System.in);
+        System.out.print("Guess a number:");
+        int guess = num.nextInt();
+        while(guess != 5){
+            System.out.print("Guess again:");
+            guess = num.nextInt();
+        }
+        System.out.println("You guessed correctly!");
+    }
+}
