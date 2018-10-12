7978f4c85e1d9febef84120f53ab4901f9e2e6b1
BT_1N3_04 <baishakicodes>
2018-10-12 08:43:21 -0400

    added in the amount of tries taken by both me and the computer.need to fix one more thing.

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -9,29 +9,36 @@ public class Game {
     public static void main(String[] args) {
         // write your code here
         Scanner hint = new Scanner(System.in);
-        int myNum = 35;
+        System.out.println("Pick a secret number from 1 to 100 and I'll try to guess it!");
+        int myNum = hint.nextInt();
         int max = 100;
         int min = 1;
         int compGuess = 50;
         int tries = 0;
+        System.out.println("Is your number "+compGuess+"?");
         while (myNum!=compGuess) {
-            System.out.println("Is your number "+compGuess+"?");
             String userHint = hint.nextLine();
             if (userHint.equals("Too high")) {
                 max = compGuess;
                 compGuess = (min+max)/2;
+                System.out.println("Is your number "+compGuess);
                 tries++;
             } else {
                 if (userHint.equals("Too low")) {
                     min = compGuess;
-                    compGuess = (min+max)/2;
+                    compGuess = (min + max) / 2;
+                    System.out.println("Is your number "+compGuess);
                     tries++;
+
                 }
             }
         }
-        System.out.println("You guessed my number of "+myNum+"!");
-        System.out.println("The computer took "+tries+" guesses to get my number!");
+        System.out.println("Your number is "+compGuess+"!");
+        System.out.println("And I took "+tries+" guesses to get your number!");
     }
 }
+
+
+
 
 
