c32fd3db58909a131f4fe82a89f6722397dc353c
BT_1N3_04 <baishakicodes>
2018-10-12 08:21:57 -0400

    added in the amount of tries taken by both me and the computer.

--- src/com/company/CompGenGame.java
+++ src/com/company/CompGenGame.java
@@ -9,10 +9,13 @@ public class CompGenGame {
         Scanner num = new Scanner(System.in);
         System.out.println("Guess a number between 1 and 10:");
         int guess = num.nextInt();
+        int tries = 0;
         while (guess != compNum) {
             System.out.println("That's not correct! Guess Again:");
             guess = num.nextInt();
+            tries++;
         }
         System.out.println("You guessed correctly!");
+        System.out.println("You took "+tries+" tries to guess my number correctly!");
     }
 }
--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -13,20 +13,24 @@ public class Game {
         int max = 100;
         int min = 1;
         int compGuess = 50;
+        int tries = 0;
         while (myNum!=compGuess) {
             System.out.println("Is your number "+compGuess+"?");
             String userHint = hint.nextLine();
             if (userHint.equals("Too high")) {
                 max = compGuess;
                 compGuess = (min+max)/2;
+                tries++;
             } else {
                 if (userHint.equals("Too low")) {
                     min = compGuess;
                     compGuess = (min+max)/2;
+                    tries++;
                 }
             }
         }
         System.out.println("You guessed my number of "+myNum+"!");
+        System.out.println("The computer took "+tries+" guesses to get my number!");
     }
 }
 
