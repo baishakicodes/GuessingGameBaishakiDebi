548669e64e51906ac63b490712b998e1bdb68179
baishakicodes <43207472+baishakicodes@users.noreply.github.com>
2018-10-11 22:14:13 -0400

    Finally fixed the AI guessing game. Whew!!

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -1,18 +1,33 @@
 package com.company;
 
+import sun.font.TrueTypeFont;
+
 import java.util.Scanner;
 
 public class Game {
 
     public static void main(String[] args) {
-	// write your code here
-        int AINum = (int)(Math.random()*100+1);
-        int compGuess = (int)(Math.random()*100+1);
+        // write your code here
         Scanner hint = new Scanner(System.in);
-        String userHint = hint.nextLine();
-        while(AINum != compGuess){
-            
+        int myNum = 35;
+        int max = 100;
+        int min = 1;
+        int compGuess = 50;
+        while (myNum!=compGuess) {
+            System.out.println("Is your number "+compGuess+"?");
+            String userHint = hint.nextLine();
+            if (userHint.equals("Too high")) {
+                max = compGuess;
+                compGuess = (min+max)/2;
+            } else {
+                if (userHint.equals("Too low")) {
+                    min = compGuess;
+                    compGuess = (min+max)/2;
+                }
+            }
         }
-
+        System.out.println("You guessed my number of "+myNum+"!");
     }
 }
+
+
